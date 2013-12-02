a3b7789b8736f4efcaa375494964f3ce196f7e00
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-12-02 15:12:42 +0100

    Use tool runnerr

--- newspaper-hadoop-jpylyzer-job/src/main/java/dk/statsbiblioteket/medieplatform/hadoop/JpylyzerJob.java
+++ newspaper-hadoop-jpylyzer-job/src/main/java/dk/statsbiblioteket/medieplatform/hadoop/JpylyzerJob.java
@@ -8,29 +8,31 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
 
-public class JpylyzerJob {
-	
-	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException{
-        int code = new JpylyzerJob().runJob(args[0], args[1]);
-        System.exit(code);
-	}
+public class JpylyzerJob implements Tool {
 
-    public int runJob(String inputFiles, String outputFolder) throws
-                                                               IOException,
-                                                               ClassNotFoundException,
-                                                               InterruptedException {
-        Job job = Job.getInstance();
+    private Configuration conf;
+
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new JpylyzerJob(), args);
+        System.exit(res);
+    }
+
+    @Override
+    public int run(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Job job = Job.getInstance(getConf());
         job.setJarByClass(JpylyzerJob.class);
         job.setMapperClass(JpylyzerMapper.class);
-        
+
         Configuration configuration = job.getConfiguration();
         configuration.setIfUnset(ConfigConstants.JPYLYZER_PATH, "jpylyzer.py");
-        configuration.setIfUnset(ConfigConstants.DOMS_URL,"http://achernar:7880/fedora");
-        configuration.setIfUnset(ConfigConstants.DOMS_USERNAME,"fedoraAdmin");
-        configuration.setIfUnset(ConfigConstants.DOMS_PASSWORD,"fedoraAdminPass");
+        configuration.setIfUnset(ConfigConstants.DOMS_URL, "http://achernar:7880/fedora");
+        configuration.setIfUnset(ConfigConstants.DOMS_USERNAME, "fedoraAdmin");
+        configuration.setIfUnset(ConfigConstants.DOMS_PASSWORD, "fedoraAdminPass");
 
 
         job.setReducerClass(DomsSaverReducer.class);
@@ -42,9 +44,19 @@ public class JpylyzerJob {
 
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        FileInputFormat.addInputPath(job, new Path(inputFiles));
-        FileOutputFormat.setOutputPath(job, new Path(outputFolder));
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
         boolean result = job.waitForCompletion(true);
         return result ? 0 : 1;
+    }
+
+    @Override
+    public Configuration getConf() {
+        return conf;
+    }
+
+    @Override
+    public void setConf(Configuration configuration) {
+        this.conf = configuration;
     }
 }
