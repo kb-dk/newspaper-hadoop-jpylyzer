0d2e366b4087bccb9dbef079b6a998f773093c33
Bolette <bam@statsbiblioteket.dk>
2013-12-03 11:39:21 +0100

    Mocked up the batch structure for the test

--- newspaper-hadoop-jpylyzer-component/src/test/java/dk/statsbiblioteket/medieplatform/autonomous/SampleRunnableComponentTest.java
+++ newspaper-hadoop-jpylyzer-component/src/test/java/dk/statsbiblioteket/medieplatform/autonomous/SampleRunnableComponentTest.java
@@ -3,6 +3,8 @@ package dk.statsbiblioteket.medieplatform.autonomous;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import static org.testng.Assert.assertTrue;
@@ -16,12 +18,17 @@ public class SampleRunnableComponentTest {
         Properties properties = new Properties();
         properties.load(new FileInputStream(pathToProperties));
 
-        properties.setProperty(JpylyzerRunnableComponent.JOB_FOLDER,"inputFiles");
-        properties.setProperty(JpylyzerRunnableComponent.PREFIX,"jpeg2k/");
+        properties.setProperty(dk.statsbiblioteket.medieplatform.hadoop.ConfigConstants.JOB_FOLDER,"inputFiles");
+        properties.setProperty(dk.statsbiblioteket.medieplatform.hadoop.ConfigConstants.PREFIX,"jpeg2k/");
         properties.setProperty(ConfigConstants.ITERATOR_USE_FILESYSTEM,"False");
         properties.setProperty(ConfigConstants.JPYLYZER_PATH,"/usr/lib/python2.7/site-packages/jpylyzer/jpylyzer.py");
 
-        JpylyzerRunnableComponent component = new JpylyzerRunnableComponent(properties);
+        JpylyzerRunnableComponent component = new JpylyzerRunnableComponent(properties){
+            @Override
+            public InputStream retrieveBatchStructure(Batch batch) throws IOException {
+                return Thread.currentThread().getContextClassLoader().getResourceAsStream("assumed-valid-structure.xml");
+            }
+        };
         ResultCollector resultCollector = new ResultCollector("crap", "crap");
         component.doWorkOnBatch(new Batch("400022028241"), resultCollector);
         assertTrue(resultCollector.isSuccess(),resultCollector.toReport());
